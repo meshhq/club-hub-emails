fabe0c95756ca90c379774394ad1dd89382ebda9
Kevin Coleman <kcoleman731@gmail.com>
2019-02-21 14:08:28 -0800

    Adding prod templates

---
+++ dist/models/club.d.ts
@@ -0,0 +1,15 @@
+export interface ClubInfo {
+    name: string;
+    domain: string;
+    logoURL: string;
+    twitterURL?: string;
+    facebookURL?: string;
+    instagramURL?: string;
+    address: AddressInfo;
+}
+export interface AddressInfo {
+    street: string;
+    city: string;
+    state: string;
+    zip: string;
+}
