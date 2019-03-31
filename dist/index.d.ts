226adf97a0ccac5face608c5fead59390cd24292
Kevin Coleman <kcoleman731@gmail.com>
2019-03-31 08:36:44 -0700

    Adding support for custom colors

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import * as core from 'club-hub-core';
-import * as CarClub from './clubs/dc-otto/mailer/index';
-export { CarClub };
-export declare const CompileGenericEmail: (content: string, club: core.Club.Model) => Promise<string>;
-export declare const CompileEventEmail: (event: core.Event.Model, club: core.Club.Model, link: string) => Promise<string>;
-export declare const CompilePostEmail: (post: core.Post.Model, club: core.Club.Model, link: string) => Promise<string>;
-export declare const CompileConfirmationEmail: (reservation: core.Event.Reservation, event: core.Event.Model, group: core.Calendar.Group, club: core.Club.Model) => Promise<string>;
