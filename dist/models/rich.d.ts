45dd65bc67e3068205f64f5ef5cb952728cdffc4
Kevin Coleman <kcoleman731@gmail.com>
2019-04-01 13:59:41 -0700

    Adding support for actions

---
+++ dist/models/rich.d.ts
@@ -0,0 +1,10 @@
+import { ClubInfo } from './club';
+export interface RichContent {
+    name?: string;
+    photoURL?: string;
+    content: string;
+    url?: string;
+    cta?: string;
+    unsubscribeURL: string;
+    club: ClubInfo;
+}
